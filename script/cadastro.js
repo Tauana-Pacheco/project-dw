6c5493d13ca600059752406c1d46b07acec423d6
YarleiCruz98 <yarleicruz1998@gmail.com>
2024-03-16 22:59:56 -0300

    Update: Validação dos dados via js, apresentação das mensagens de erros retornadas pel api, direcionamento para página de login

--- script/cadastro.js
+++ script/cadastro.js
@@ -1,7 +1,10 @@
 const url = 'https://api-go-wash-efc9c9582687.herokuapp.com/api/user';
 
-async function cadastroUsuario(){   
-    var name = document.getElementById('name');     
+const fields = document.querySelectorAll('.required')
+const spans = document.querySelectorAll('.span-required')
+
+async function cadastroUsuario(){ 
+    const name = document.getElementById('name');     
     let resposta = await fetch(url,{
         method:"POST",
         body:JSON.stringify(
@@ -23,10 +26,77 @@ async function cadastroUsuario(){
     let data = await resposta.json();
     
     
-    if(data.data.statusCode != 200){
-        alert(data.data.errors?.cpf_cnpj[0]);
-        return;
+    if(data.data.statusCode == 422 && data.data.errors.cpf_cnpj){
+        alert(data.data.errors.cpf_cnpj[0]);
+    } else if(data.data.statusCode == 422 && data.data.errors.birthday){
+        alert(data.data.errors.birthday[0]);
+    } else if(data.data.statusCode == 422 && data.data.errors.email){
+        alert(data.data.errors.email[0]);
+    } else if((data.data.includes("Enviamos um link de ativação de conta"))){
+        alert(data.data)
+        window.location.href = "login.html";
     }
-    alert("Cadastro feito com sucesso");
-    window.location.href = "login.html";
+
 }
+
+function setError(index){
+    fields[index].style.border = '2px solid #e63636'
+    spans[index].style.display = 'block'
+}
+
+function removeError(index) {
+    fields[index].style.border = '';
+    spans[index].style.display = 'none';
+}
+
+function nameValidate(){
+    if(fields[0].value.length <3){
+        setError(0)
+    }
+    else{
+        removeError(0)
+    }
+}
+
+function validateCpfCnpj() {
+    const campo = document.getElementById('cpf_cnpj');
+    const valor = campo.value.trim();
+    const caracteresPermitidos = /[0-9]/;
+
+    if (valor.length < 11) {
+        setError(1);
+    } else if (!caracteresPermitidos.test(valor)) {
+        setError(1);
+    } else {
+        removeError(1);
+    }
+}
+
+function emailValidate(){
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if(!emailRegex.test(fields[2].value)){
+        setError(2)
+    }
+    else{
+        removeError(2)
+    }
+}
+
+function passwordValidate(){
+    if(fields[3].value.length <8){
+        setError(3)
+    }
+    else{
+        removeError(3)
+    }
+}
+
+const campo = document.getElementById('cpf_cnpj');
+campo.addEventListener('keypress', function(event) {
+    const char = event.key
+    const caracteresPermitidos = /[0-9.-]/;
+    if (!caracteresPermitidos.test(char)) {
+        event.preventDefault();
+    }
+})
