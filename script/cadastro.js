c8203899d39244f5047a6abe00e5309ca9fa6272
caioadauri <caioadaurigomes@gmail.com>
2024-03-25 19:13:06 -0300

    add validacao data e mascara

--- script/cadastro.js
+++ script/cadastro.js
@@ -5,7 +5,8 @@ async function cadastroUsuario() {
   var email = document.getElementById('email')
   var password = document.getElementById('password')
   var cpf_cnpj = document.getElementById('cpf_cnpj')
-  var birthday = document.getElementById('birthday')
+  
+
 
   let response = await fetch(url, {
       method: 'POST',
@@ -17,7 +18,7 @@ async function cadastroUsuario() {
         'password': password.value,
         'cpf_cnpj': cpf_cnpj.value,
         'terms': 1,
-        'birthday': birthday.value
+        'birthday': validarCampos().dataFormatada
         }
     ),
     headers: {'Content-Type': 'application/json'}
@@ -25,10 +26,20 @@ async function cadastroUsuario() {
 
   let data = await response.json()
   
-  if (data.data.statusCode != 200) {
-    alert(data.data.errors)
-    return
-  }
+  if (data.data || data.data.statusCode ==! 200) {
+    if(data.data.errors && data.data.errors?.cpf_cnpj) {
+        return alert(data.data.errors.cpf_cnpj[0]);
+    }
+     if(data.data.errors && data.data.errors?.birthday) {
+        return alert(data.data.errors.birthday[0]);
+    }
+    if(data.data.errors && data.data.errors?.email) {
+        return alert(data.data.errors.email[0]);
+    }
+    if(!data.data.errors) {
+        window.location.href = "login.html"
+    }
+}
   console.log('Cadastro realizado com sucesso')
 }
 
@@ -43,11 +54,19 @@ function validarCampos() {
   if (nome.value == "") {
     alert('Inserir o nome')
     nome.focus()
+    return false
   }
 
   if (email.value == "") {
     alert('Inserir o email')
     email.focus()
+    return false
+  } else {
+    var regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    if (!regexEmail.test(email.value)) {
+      alert('Por favor insera um email válido')
+      return false
+    }
   }
 
   if (password.value == "") {
@@ -63,7 +82,22 @@ function validarCampos() {
   if (birthday.value == "") {
     alert('Inserir o Data de nascimento')
     birthday.focus()
+  } else {
+    var regexData = /^\d{2}\/\d{2}\/\d{4}$/;
+    if (!regexData.test(birthday.value)) {
+      alert('Insira a data nesse formato 16/01/1990')
+      return false
+    }
   }
+
+  var partesData = birthday.value.split('/')
+  var dia = partesData[0];
+  var mes = partesData[1];
+  var ano = partesData[2];
+  var dataFormatada = ano + '-' + mes + '-' + dia;
+  console.log(dataFormatada)
+
+  
 
   if ( checkbox.checked) {
     console.log('termos aceito')
@@ -71,4 +105,9 @@ function validarCampos() {
     alert('Aceitar os termos de uso')
     checkbox.focus()
   }
+
+  return true
 }
+
+
+
