3f107f68a8492755b3bbdc68fab3d97a126f2848
YarleiCruz98 <yarleicruz1998@gmail.com>
2024-05-09 12:57:54 -0300

    Resolvendo problema de duplicação de chamdas da API e resolvendo problema na exibição de erros

--- script/login.js
+++ script/login.js
@@ -1,84 +1,72 @@
 const url = "https://go-wash-api.onrender.com/api/login";
 
-async function accessEmail() {
-  try{
-    let email = document.getElementById("email").value;
-    let password = document.getElementById("password").value;
-    let user_type_id = 1
-    
-    let loginButton = document.getElementById("loginButton");
-    loginButton.disabled = true;
+async function accessEmail(email, password) {
+  let user_type_id = 1
+  let loginButton = document.getElementById("loginButton");
+  
+  loginButton.disabled = true;
 
-    let response = await fetch(url, {
-      method: "POST",
-      body: JSON.stringify({
-        email,
-        password,
-        user_type_id
-      }),
-      headers: { "Content-Type": "application/json" },
-    })
+  let response = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify({
+      email,
+      password,
+      user_type_id
+    }),
+    headers: { "Content-Type": "application/json" },
+  })
 
-    let data = await response.json();
-
-    if (!response.ok) {
-      if (data && data.data && data.data.errors === "Usuário não esta ativo") {
-        alert("Erro de login: Usuário não está ativo");
-        return false
-      } 
-      const errors = data.data.errors;
-      for (const field in errors) {
-        alert(`Erro para '${field}': ${errors[field][0]}`);
-      }
-      loginButton.disabled = false
-      return false; 
+  let data = await response.json();
+  const dataError = data && data.data && data.data.errors
+  if (!response.ok) {
+    if (dataError === "Usuário não esta ativo") {
+      alert("Erro de login: Usuário não está ativo");
+      return false
+    } else if(dataError === "Usuário não foi encontrado") {
+      alert("Erro de login: Usuário não está cadastrado");
+      return false
     }
-    return true;
-  }catch (error) {
-    console.error("Erro durante o login:", error);
     loginButton.disabled = false
-    return false;
+    alert("Erro ao realizar o login do usuario. Tente novamente");
+    return false; 
   }
-
+  return true;
 }
 
-function fieldValidation() {
-  const email = form.email;
-  const password = form.password;
+function fieldValidation(email, password) {
   const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-  if (email.value == "") {
-    alert("Inserir o email");
-    email.focus();
+  if (!email) {
+    alert("Insira o email");
     return false;
-  } else if (!regexEmail.test(email.value)) {
+  } else if (!regexEmail.test(email)) {
     alert("Por favor insira um email válido");
-  } else {
     return false;
   }
 
-  if (password.value == "") {
-    alert("Inserir a senha");
-    password.focus();
-  } else if (password.value.length < 6) {
+  if (!password) {
+    alert("Insira a senha");
+    return false;
+  } else if (password.length < 6) {
     alert("Senha com no mínimo 6 caracteres");
-    nome.focus();
-  } else {
     return false;
   }
+
+  return true;
 }
 
 async function handleLogin() {
-  if (accessEmail()) {
-    try {
-      let loginSuccessful = await accessEmail();
-      if (loginSuccessful) {
-        window.location.href = "home.html";
-      } else {
-        console.log("O login falhou");
-      }
-    } catch (error) {
-      console.error("Erro durante o login:", error);
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  if (fieldValidation(email, password)) {
+    const success = await accessEmail(email, password);
+
+    if (success) {
+      alert("Login realizado com sucesso!");
+      window.location.href = "home.html";
+    } else {
+      loginButton.disabled = false
     }
   }
 }
